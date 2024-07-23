6a9a7a5a826c5a205179cb5708381808d6537017
CeciJhoana <ceciliavergara997@gmail.com>
2024-07-22 23:41:41 -0400

    Change[registro/models]:Add clase usuario

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    nombre_usuario = models.CharField(max_length=100, unique=True)
+    edad = models.IntegerField()
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nombre_usuario
