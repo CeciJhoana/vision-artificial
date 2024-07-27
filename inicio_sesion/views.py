20016abe45e99971e843e8e6bfedea892758d450
CeciJhoana <ceciliavergara997@gmail.com>
2024-07-27 11:24:03 -0400

    feat iniciar sesion

--- inicio_sesion/views.py
+++ inicio_sesion/views.py
@@ -1,3 +1,201 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import StreamingHttpResponse, JsonResponse
+from django.urls import reverse
+import cv2
+import numpy as np
+import math
+import face_recognition as fr
+import mediapipe as mp
+import mediapipe.python.solutions.face_mesh_connections as face_mesh_connections
+from registro.models import Foto, Usuario
 
+# Variable global para almacenar el estado de éxito
+success_status = {}
 # Create your views here.
+
+def iniciar_sesion(request):
+    exito = request.GET.get('exito')
+    return render(request, 'inicio_sesion.html', {'exito': exito})
+
+def comparar_fotos(faces, facescode):
+    fotos = Foto.objects.all()
+    known_face_encodings = [fr.face_encodings(fr.load_image_file(foto.imagen.path))[0] for foto in fotos]
+    known_face_ids = [foto.usuario.id for foto in fotos]
+
+    for face_encoding in facescode:
+        matches = fr.compare_faces(known_face_encodings, face_encoding)
+        face_distances = fr.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+
+        if matches[best_match_index]:
+            return known_face_ids[best_match_index]
+    return None
+
+                                                    
+    
+
+def video_feed(request):
+    return StreamingHttpResponse(capture_and_compare_image(request), content_type='multipart/x-mixed-replace; boundary=frame')
+
+# Inicializa los objetos y configuraciones para la detección de rostros y la malla facial
+def initialize_face_detection():
+    # Heramienta de dibujo
+    mpDraw = mp.solutions.drawing_utils
+    ConfigDraw = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
+    #Objeto de la malla facial
+    FacemeshObject = mp.solutions.face_mesh
+    FaceMesh = FacemeshObject.FaceMesh(max_num_faces=1)
+    #Objeto de detector de rostro
+    FaceObject = mp.solutions.face_detection
+    detector = FaceObject.FaceDetection(min_detection_confidence=0.5, model_selection=1)
+    return mpDraw, ConfigDraw, FaceMesh, detector
+
+# Procesa la imagen para detectar la malla facial y dibujar los puntos de la malla en la imagen
+def process_face_mesh(frame, FaceMesh, mpDraw, ConfigDraw):
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    res = FaceMesh.process(rgb)
+    if res.multi_face_landmarks:
+        # Extraer la malla de rostro
+        for rostros in res.multi_face_landmarks:
+            #Dibujar
+            mpDraw.draw_landmarks(frame, rostros, face_mesh_connections.FACEMESH_TESSELATION, ConfigDraw, ConfigDraw)
+    return res
+
+# Obtiene las coordenadas de los puntos clave de la malla facial
+def get_landmark_coordinates(res, frame):
+    px, py, lista = [], [], []
+    if res.multi_face_landmarks:
+        for rostros in res.multi_face_landmarks:
+            #Extraer puntos clave
+            for id, puntos in enumerate(rostros.landmark):
+                #informacion de la imagen
+                alto, ancho, _ = frame.shape
+                x, y = int(puntos.x * ancho), int(puntos.y * alto)
+                px.append(x)
+                py.append(y)
+                lista.append([id, x, y])
+    return px, py, lista
+
+# Calcula las distancias entre puntos clave específicos de la malla facial
+def calculate_distances(lista):
+    # Ojo derecho
+    x1, y1 = lista[145][1:]
+    x2, y2 = lista[159][1:]
+    longitud1 = math.hypot(x2 - x1, y2 - y1)
+    # Ojo Izquierdo
+    x3, y3 = lista[374][1:]
+    x4, y4 = lista[386][1:]
+    longitud2 = math.hypot(x4 - x3, y4 - y3)
+    return longitud1, longitud2
+
+# Detecta rostros en la imagen usando el detector de MediaPipe
+def detect_faces(frame, detector, rgb):
+    return detector.process(rgb)
+
+# Maneja la detección de rostros, aplicando offsets y dibujando el cuadro de detección en la imagen
+def handle_face_detection(faces, frame, ancho, alto, offsetx, offsety, confThreshold):
+    xi = yi = an = al = xf = yf = 0
+    for face in faces.detections:
+        score = face.score[0]
+        bbox = face.location_data.relative_bounding_box
+        if score > confThreshold:
+             #Convertir a pixeles los valores normalizados de bbox
+            xi, yi, an, al = int(bbox.xmin * ancho), int(bbox.ymin * alto), int(bbox.width * ancho), int(bbox.height * alto)
+            #Aplicar offset en x
+            offsetan = int((offsetx / 100) * an)
+            xi = int(xi - offsetan / 2)
+            an += offsetan
+            xf = xi + an
+            #Aplicar offset en y
+            offsetal = int((offsety / 100) * al)
+            yi = int(yi - offsetal)
+            al += offsetal
+            yf = yi + al
+            cv2.rectangle(frame, (xi, yi, an, al), (255, 0, 255), 2)
+    return xi, yi, an, al, xf, yf
+
+
+def capture_and_compare_image(request):
+    # Usa la cámara del dispositivo
+    camera = cv2.VideoCapture(0)
+    if not camera.isOpened():
+        return JsonResponse({'error': 'No se puede abrir la cámara.'})
+
+    parpadeo = False
+    conteo = 0
+    step=0
+    # Offset
+    offsety = 40
+    offsetx = 20
+    #tumbral de presición
+    confThreshold = 0.5
+    mpDraw, ConfigDraw, FaceMesh, detector = initialize_face_detection()
+    
+    
+   
+
+    while True:
+        success, frame = camera.read()
+        if not success or frame is None:
+            print("Error al capturar el frame de la cámara.")
+            continue
+
+        frameSave = frame.copy()
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        res = process_face_mesh(frame, FaceMesh, mpDraw, ConfigDraw)
+        #lista de resuktados
+        px, py, lista = get_landmark_coordinates(res, frame)
+
+        if len(lista) == 468:
+            longitud1, longitud2 = calculate_distances(lista)
+            faces = detect_faces(frame, detector, rgb)
+            if faces.detections is not None:
+                xi, yi, an, al, xf, yf = handle_face_detection(faces, frame, frame.shape[1], frame.shape[0], offsetx, offsety, confThreshold)
+                x7, y7 = lista[70][1:]
+                x8, y8 = lista[300][1:]
+
+                # Caso en el que el rostro mira al frente
+                if x7 > lista[139][1] and x8 < lista[368][1]:
+                    # Contador de Parpadeos
+                    if longitud1 <= 10 and longitud2 <= 10 and not parpadeo:
+                        conteo += 1
+                        parpadeo = True
+                    elif longitud1 > 10 and longitud2 > 10 and parpadeo:
+                        parpadeo = False
+                    
+                    # Condicion si parpadea 3 o hasta mayor a tres
+                    if conteo >= 3:
+
+                        cv2.rectangle(frame, (xi, yi, an, al), (0, 255, 0), 2)
+                        # condicion para sacar foto con los ojos abiertos
+                        if longitud1 > 15 and longitud2 > 15:
+                            faces = fr.face_locations(rgb)
+                            facescode = fr.face_encodings(rgb, faces)
+                            usuario_id = comparar_fotos(faces, facescode)
+                            if usuario_id:
+                                print("----------Estamos aqui---------------")
+                                print (usuario_id)
+                                request.session['usuario_id'] = usuario_id
+                                success_status[usuario_id] = True
+                            break
+                            
+                else:
+                    conteo = 0
+        
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
+        
+    
+    camera.release() # Libera el recurso de la cámara   
+    cv2.destroyAllWindows()
+
+#verifica si la imagen se ha guardado correctamente.
+def check_success(request):
+    
+    usuario_id = request.session.get('usuario_id')
+    if usuario_id and success_status.get(usuario_id):
+        return JsonResponse({'exito': True})
+    return JsonResponse({'exito': False})
